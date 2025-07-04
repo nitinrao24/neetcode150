debdce21aca0c0f5d0e8517beb81cdf6432333cb
nitinrao24 <nitinrao2025@gmail.com>
2025-07-04 15:28:38 -0700

    Delete 18. Bit_Manipulation/reversebits directory

--- 18. Bit_Manipulation/reversebits/reversebits.py
+++
@@ -1,8 +0,0 @@
-# Leetcode 190
-# Reverse bits of a given 32 bits unsigned integer.
-def reverseBits(binarynumber):
-    bits = binarynumber[::-1]
-    return int(bits, 2)
-
-print(reverseBits("00000010100101000001111010011100"))
-print(reverseBits("11111111111111111111111111111101"))
