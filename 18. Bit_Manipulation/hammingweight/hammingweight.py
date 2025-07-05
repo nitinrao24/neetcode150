fb58ceb4c45856ebb7be05f58bc3dfe827071aad
nitinrao24 <nitinrao2025@gmail.com>
2025-07-05 15:31:17 -0700

    Add files via upload

--- 18. Bit_Manipulation/hammingweight/hammingweight.py
+++ 18. Bit_Manipulation/hammingweight/hammingweight.py
@@ -1,1 +1,15 @@
+# leetcode 191
+# Given a positive integer n,
+# write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
 
+# Time Complexity:
+# Space Complexity:
+def hammingWeight(num):
+    count = 0
+    binum = bin(num)[2:]
+    for i in binum:
+        if i == '1':
+            count += 1
+    return count
+
+print(hammingWeight(2147483645))
