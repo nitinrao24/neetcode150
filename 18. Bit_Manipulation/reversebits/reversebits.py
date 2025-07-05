2d238e83bab1cb11f72031e0626066d80663d4e7
nitinrao24 <nitinrao2025@gmail.com>
2025-07-05 15:34:22 -0700

    Add files via upload

--- 18. Bit_Manipulation/reversebits/reversebits.py
+++ 18. Bit_Manipulation/reversebits/reversebits.py
@@ -1,1 +1,11 @@
+# Leetcode 190
+# Reverse bits of a given 32 bits unsigned integer.
 
+# Time Complexity:
+# Space Complexity:
+def reverseBits(binarynumber):
+    bits = binarynumber[::-1]
+    return int(bits, 2)
+
+print(reverseBits("00000010100101000001111010011100"))
+print(reverseBits("11111111111111111111111111111101"))
