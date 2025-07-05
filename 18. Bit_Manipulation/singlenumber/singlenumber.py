71a510c1c7ecf9de550ea106eefe79d711b9ccd2
nitinrao24 <nitinrao2025@gmail.com>
2025-07-05 15:32:24 -0700

    Add files via upload

--- 18. Bit_Manipulation/singlenumber/singlenumber.py
+++ 18. Bit_Manipulation/singlenumber/singlenumber.py
@@ -1,1 +1,15 @@
+# leetcode 136
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
+# Time Complexity:
+# Space Complexity:
+def singleNumber(nums):
+    seen = set()
+    for n in nums:
+        if n in seen:
+            seen.remove(n)
+        else:
+            seen.add(n)
+    return seen.pop()
+
+print(singleNumber([1]))
