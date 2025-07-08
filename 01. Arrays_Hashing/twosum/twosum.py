3bf3f97cc3511b0ae4c35b6c736ee05a12b03f9f
nitinrao24 <nitinrao2025@gmail.com>
2025-07-08 15:10:29 -0700

    Add files via upload

--- 01. Arrays_Hashing/twosum/twosum.py
+++ 01. Arrays_Hashing/twosum/twosum.py
@@ -1,1 +1,20 @@
+# leetcode 1
+# Given an array of integers nums and an integer target,
+# return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
 
+# Time Complexity:
+# Space Complexity:
+
+def twoSum(nums, target):
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+print(twoSum([2,7,11,15],9))
+print(twoSum([3,2,4],6))
+print(twoSum([3,3],6))
