74beb4b42c6281730351d80c01a381ab99931283
nitinrao24 <nitinrao2025@gmail.com>
2025-07-09 12:25:42 -0700

    initial commit

---
+++ 04. Stacks/generateparantheses/generateparantheses.py
@@ -0,0 +1,23 @@
+# leetcode 22
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+# Time Complexity:
+# Space Complexity:
+
+def generateParantheses(n):
+    results = []
+
+    def rucursion(val, front, back):
+        if len(val) == 2 * n:
+            results.append(val)
+            return
+        if front < n:
+            rucursion(val + "(", front + 1, back)
+        if front > back:
+            rucursion(val + ")", front, back + 1)
+
+    rucursion("", 0, 0)
+    return results
+
+print(generateParantheses(3))
+print(generateParantheses(1))
