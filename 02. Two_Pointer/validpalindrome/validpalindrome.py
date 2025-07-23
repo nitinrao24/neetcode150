ffb19205e4c513da9301f9079728d3d45f5a700c
nitinrao24 <nitinrao2025@gmail.com>
2025-07-23 12:35:36 -0700

    initial commit

--- 02. Two_Pointer/validpalindrome/validpalindrome.py
+++ 02. Two_Pointer/validpalindrome/validpalindrome.py
@@ -4,9 +4,25 @@
 # it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 # Given a string s, return true if it is a palindrome, or false otherwise.
 
-# Time Complexity:
-# Space Complexity:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
 def validPalindrome(s):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return True
+
+
+def validPalindrome1(s):
     cleanstring = ''.join(char for char in s if char.isalnum())
     cleanstring = cleanstring.lower()
     start = 0
